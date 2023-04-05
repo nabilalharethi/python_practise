59493518b75f9cc1e00a2692831072a2d47a7bca
Nabil Abdulbari Abdulqader Al-Harethi <na223bj@student.lnu.se>
2023-04-05 04:37:27 +0200

    Task 3

---
+++ Loops.py
@@ -0,0 +1,19 @@
+""" 
+The provided code stub reads and integer, , from STDIN. For all non-negative integers les than n 
+
+
+ """
+ 
+n=int(input("Enter a number: "))
+
+
+lis =[]
+
+for i in range(n):
+    lis.append(i)
+
+
+for x in range (len(lis)):
+    print(x**2)
+    
+
